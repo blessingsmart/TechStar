7dd42d016a41a5dcd27a4f780ca35f590b0cdc45
blessingsmart <bublessingsmart@gmail.com>
2023-09-02 13:43:29 +0100

    modified:   src/component/nav.jsx

--- src/component/nav.jsx
+++ src/component/nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <div className='w-full h-10'>
-        <div className='text-red-500'>nav</div>
+        <div className=''>nav</div>
     </div>
   )
 }
