06ccab3ff76198f5aa52764098904302ba555e31
blessingsmart <bublessingsmart@gmail.com>
2023-09-08 02:35:47 +0100

    new file:   public/images/Backend.png 	new file:   public/images/Front-end.png 	new file:   public/images/Product-design.png 	new file:   public/images/js.png 	modified:   src/component/stack.jsx

--- src/component/stack.jsx
+++ src/component/stack.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
+import Product from "/images/Product-design.png"
+import Js from "/images/js.png"
+import Backend from "/images/Backend.png"
+import Frontend from "/images/Front-end.png"
 
 const Stack = () => {
   return (
-    <div className='flex flex-col text-white w-screen p-20'> 
+    <div className='flex flex-col  w-screen p-20'> 
         <div className='flex justify-between items-center'>
            <p>Find your stack and join the legion of creatives</p> 
            <p>See all communities</p> 
         </div>
-        <div className='flex gap-5'>
+        <div className='flex text-white gap-5'>
             <div className='flex flex-col gap-5'>
-                <div>Product Design</div>
-                <div className='flex gap-5'>
-                    <div>Front End Jackies</div>
-                    <div>JS Gurus</div>
+                <div className=" flex ">
+                  <img src={Product} alt="" /></div>
+                <div className='flex gap-5 '>
+                    <div className="">
+                      <img src={Frontend} alt="" /></div>
+                    <div className="">
+                      <img src={Js} alt="" /></div>
                 </div>
             </div>
-            <div>Backend Devs</div>
+            <div className="">
+              <img src={Backend} alt="" /></div>
         </div>
     </div>
   )
