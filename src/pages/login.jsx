71ec3088db18c76920896884a379037dc0894221
blessingsmart <bublessingsmart@gmail.com>
2023-09-09 08:40:21 +0100

    modified:   src/pages/login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -13,23 +13,28 @@ const Login = () => {
         <div>It feels good to have you back 
           <br /><span>Let the colabo continue ......</span>
           </div>
-        <div className='flex  p-5'>
+        <div className='flex p-5'>
               <div className='flex'>
-                <div className='flex items-center gap-5 flex-col'>
+                <div className='flex items-center gap-10 flex-col'>
                   <div> <img src={FigmaIcon} alt="" /></div>
                   <div><img src={Git} alt="" /></div>
                   <div><img src={Gmail} alt="" /></div>
-                  <div>sign in with slack</div>
                 </div>
-                <div className='flex items-center gap-5 flex-col'>
+                <div className='flex items-center gap-10 flex-col px-5'>
                   <div className='flex'> sign in with figma</div>
                   <div className='flex'>sign in with Github</div>
                   <div className='flex'>sign in with google</div>
-                  <div>sign in with slack</div>
                 </div>
               </div>
-          <div className='flex flex-col'>
-            E
+          <div className='flex gap-10 flex-col'>
+            <label htmlFor="Email" className=" text-lg">Email Address</label> 
+            <input type="text"
+                   placeholder="Enter your email address"
+                   className=" border focus:outline-none focus:border-teal-300 focus:ring-1 focus:ring-teal-300" />
+            <label htmlFor="Password" className=" text-lg">PassWord</label> 
+            <input type="Password"
+                   placeholder="Enter your Password here"
+                   className=" border focus:outline-none focus:border-teal-300 focus:ring-1 focus:ring-teal-300" />
             <button className='flex items-center bg-blue-950 hover:bg-blue-800 rounded-lg w-fit py-2 px-6 text-white text-sm md:text-base'>
                         Join our community <BsArrowRight size={20} className='mx-2'/>
             </button>
